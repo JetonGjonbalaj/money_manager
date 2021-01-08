647c54f8512f0850e38fed676c9fe2125553df75
Jeton Gjonbalaj <jetonmgjonbalaj@gmail.com>
2021-01-08 17:08:57 +0300

    Added repositories
    repositories for category and record

---
+++ back_end/MoneyManagement.Application/Interfaces/ICategoryRepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿using MoneyManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Application.Interfaces
+{
+    public interface ICategoryRepositoryAsync : IRepositoryAsync<Category>
+    {
+        Task<bool> HasUniqueNameAsync(string name);
+        Task<bool> CategoryIdExistsAsync(string id);
+    }
+}
---
+++ back_end/MoneyManagement.Application/Interfaces/IRecordRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using MoneyManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Application.Interfaces
+{
+    public interface IRecordRepositoryAsync : IRepositoryAsync<Record>
+    {
+        //Task<Record> GetUserRecordAsync(string userId);
+        Task<Expense> GetExpenseAsync(string userId, string expenseId);
+        Task AddExpenseAsync(string userId, Expense expense);
+        Task UpdateExpenseAsync(string userId, Expense expense);
+        Task DeleteExpenseAsync(string userId, Expense expense);
+    }
+}
