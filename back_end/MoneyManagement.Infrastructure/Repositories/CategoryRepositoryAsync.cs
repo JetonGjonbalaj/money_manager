91dc496b3c83682452bf43114326067c3fb314dc
Jeton Gjonbalaj <jetonmgjonbalaj@gmail.com>
2021-01-08 17:10:30 +0300

    Added repositories implementation
    Implementation for category and record repository

---
+++ back_end/MoneyManagement.Infrastructure/Repositories/CategoryRepositoryAsync.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyManagement.Application.Interfaces;
+using MoneyManagement.Domain.Entities;
+using MoneyManagement.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Infrastructure.Repositories
+{
+    public class CategoryRepositoryAsync : RepositoryAsync<Category>, ICategoryRepositoryAsync
+    {
+        private readonly DbSet<Category> _categories;
+
+        public CategoryRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _categories = dbContext.Set<Category>();
+        }
+
+        public async Task<bool> CategoryIdExistsAsync(string id)
+        {
+            return await _categories.AnyAsync(c => c.Id == id);
+        }
+
+        public async Task<bool> HasUniqueNameAsync(string name)
+        {
+            return await _categories.AllAsync(c => c.Name != name);
+        }
+    }
+}
---
+++ back_end/MoneyManagement.Infrastructure/Repositories/RecordRepositoryAsync.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyManagement.Application.Interfaces;
+using MoneyManagement.Domain.Entities;
+using MoneyManagement.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Infrastructure.Repositories
+{
+    public class RecordRepositoryAsync : RepositoryAsync<Record>, IRecordRepositoryAsync
+    {
+        private readonly DbSet<Record> _records;
+
+        public RecordRepositoryAsync (ApplicationDbContext _dbContext) : base(_dbContext)
+        {
+            _records = _dbContext.Set<Record>();
+        }
+
+        private async Task<Record> GetUserRecordAsync(string userId)
+        {
+            return await _records.Include(r => r.Expenses).ThenInclude(e => e.ExpenseCategory).Include(r => r.Incomes).SingleOrDefaultAsync(r => r.UserId == userId);
+        }
+
+        public async Task<Expense> GetExpenseAsync(string userId, string expenseId)
+        {
+            var record = await GetUserRecordAsync(userId);
+            var expense = record.Expenses.SingleOrDefault(e => e.Id == expenseId);
+
+            return expense;
+        }
+
+        public async Task AddExpenseAsync(string userId, Expense expense)
+        {
+            var record = await GetUserRecordAsync(userId);
+
+            if (record == null)
+            {
+                record = new Record();
+                record.UserId = userId;
+                await AddAsync(record);
+            }
+
+            record.Expenses.Add(expense);
+            await UpdateAsync(record);
+        }
+
+        public async Task UpdateExpenseAsync(string userId, Expense expense)
+        {
+            var record = await GetUserRecordAsync(userId);
+            var oldExpense = record.Expenses.SingleOrDefault(e => e.Id == expense.Id);
+
+            record.Expenses.Remove(oldExpense);
+            record.Expenses.Add(expense);
+            await UpdateAsync(record);
+        }
+
+        public async Task DeleteExpenseAsync(string userId, Expense expense)
+        {
+            var record = await GetUserRecordAsync(userId);
+
+            record.Expenses.Remove(expense);
+            await UpdateAsync(record);
+        }
+    }
+}
