5be110c06687c0743383767755812cb5bfe65b66
FoOok4 <fitim0306@gmail.com>
2021-01-08 20:53:05 +0300

    Created validators
    added category command validator
    added expense command validator

---
+++ back_end/MoneyManagement.Application/Features/CategoryFeatures/Commands/DeleteCategory/DeleteCategoryCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MoneyManagement.Application.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Application.Features.CategoryFeatures.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommandValidator : AbstractValidator<DeleteCategoryCommand>
+    {
+        public DeleteCategoryCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty().WithMessage(ValidationConstants.RequiredText);
+        }
+    }
+}
---
+++ back_end/MoneyManagement.Application/Features/RecordFeatures/Commands/DeleteExpense/DeleteExpenseCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MoneyManagement.Application.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement.Application.Features.RecordFeatures.Commands.DeleteExpense
+{
+    public class DeleteExpenseCommandValidator : AbstractValidator<DeleteExpenseCommand>
+    {
+        public DeleteExpenseCommandValidator()
+        {
+            RuleFor(e => e.Id)
+                .NotEmpty().WithMessage(ValidationConstants.RequiredText);
+        }
+    }
+}
