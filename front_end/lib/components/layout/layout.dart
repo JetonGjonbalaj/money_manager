cfbe75758a49bfab6ef12230ef3c5006e3013e9b
FoOok4 <fitim0306@gmail.com>
2021-02-18 07:40:17 +0100

    changed loayout

--- front_end/lib/components/layout/layout.dart
+++ front_end/lib/components/layout/layout.dart
@@ -5,12 +5,22 @@ import '../../utils/constants.dart';
 
 class Layout extends StatelessWidget {
   final Widget child;
+  final bool hasBackButton;
 
-  Layout({this.child});
+  Layout({this.child, this.hasBackButton=false});
 
   @override
   Widget build(BuildContext context) {
+    AppBar appBar;
+
+    if(hasBackButton) {
+      appBar = AppBar(
+        automaticallyImplyLeading: true,
+      );
+    }
+
     return Scaffold(
+      appBar: appBar,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.only(left: defaultPadding, right: defaultPadding, top: defaultPadding),
